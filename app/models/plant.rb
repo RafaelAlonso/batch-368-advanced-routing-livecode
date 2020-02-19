9b97f930b20d6506356664a5c60b4409558602dc
Rafael Alonso <rafael.palonso@gmail.com>
2020-02-19 18:52:51 -0300

    Livecode batch-368

---
+++ app/models/plant.rb
@@ -0,0 +1,7 @@
+class Plant < ApplicationRecord
+  belongs_to :garden
+
+  validates :name, presence: true, uniqueness: true
+  validates :image_url, presence: true
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :gardens, only: [ :show ] do
+    resources :plants, only: [ :new, :create ]
+  end
+
+  root to: 'gardens#index'
+
 end
